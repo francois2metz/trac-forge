2cf8aa9ba2c772f71f63bcf42cf9257c35764e71
Stephan Gorget <phantez@gmail.com>
2008-03-20 16:05:48 +0000

    tracforge has been ported to jinja-1.2-1
    git-svn-id: http://forge.2metz.fr/svn/trac-forge/trunk@6 671e0883-ef23-4d3e-8a5f-78a0a4f73774

--- projects.py
+++ projects.py
@@ -1,23 +1,26 @@
 from mod_python import apache
 from trac.env import open_environment
 from dircache import listdir
-from jinja import Template, Context, FileSystemLoader
+from jinja import Environment, FileSystemLoader
 import time
 from os import path
 
 class Config:
-	"Contient la configuration"
-	forge_href = "/"
-	trac_href = "/"
-	trac_dir = ""
+	"""Contient la configuration"""
+	root_dir = "/var/www/tracforge"
+	forge_href = "/tracforge"
+	forge_dir = root_dir+"/tracforge"
+	template_dir = forge_dir+"/templates"
+	media_href = "/media/"
+	trac_href = "/trac/"
+	trac_dir = root_dir+"/trac"
 	demo_href = "/demo"
-	demo_dir = ""
-	archives_href = "/archives"
-	archives_dir = ""
-	template_dir = "templates/"
+	demo_dir = root_dir+"/demo"
+	archives_href = "/archive"
+	archives_dir = root_dir+"/archive"
 
 class TracProject:
-	"Instance d'un projet trac"
+	""""Instance d'un projet trac"""
 	def __init__(self, path):
 		self.path = path
 		self.env = open_environment(path)
@@ -27,7 +30,7 @@ class TracProject:
 		return self.env.get_repository()
 
 class TracProjects:
-	"Tous les projets trac"
+	""""Tous les projets trac"""
 	def __init__(self, trac_dir):
 		self.trac_dir = trac_dir
 		self._projects = self._get_projects()
@@ -51,11 +54,11 @@ class TracProjects:
 		return tracprojects
 
 def handler(req):
-	"traitement de la requete"
+	"""traitement de la requete"""
 	config = Config()
-	if req.uri == "/admin":
+	if req.uri == config.forge_href+"/admin":
 		view_admin(req, config)
-	elif req.uri == "/a" or req.uri == "/z" or req.uri == "/":
+	elif req.uri == config.forge_href+"/a" or req.uri == config.forge_href+"/z" or req.uri == config.forge_href+"/":
 		view_index(req, config)
 	else:
 		view_404(req, config)
@@ -63,100 +66,130 @@ def handler(req):
 
 def view_admin(req, config):
 	req.content_type = 'text/html'
-	t = Template('admin', FileSystemLoader(config.template_dir))
-	c = Context({
+	env = Environment(loader=FileSystemLoader(config.template_dir))
+	tmpl = env.get_template("admin.html")
+	context = {
 		'users' : [{'name' : 'toto'}],
-		'forge_href' : config.forge_href
-	})
-	req.write(t.render(c))
+		'forge_href' : config.forge_href,
+		'media_href' : config.media_href
+	}
+	req.write(tmpl.render(context))
 
 def view_404(req, config):
-	"Page d'erreur 404"
+	"""Page d'erreur 404"""
 	req.content_type = 'text/html'
 	req.status = 404
-	t = Template('404', FileSystemLoader(config.template_dir))
-	c = Context({
+	env = Environment(loader=FileSystemLoader(config.template_dir))
+	tmpl = env.get_template("404.html")
+	context = {
 		'forge_href' : config.forge_href
-	})
-	req.write(t.render(c))
+	}
+	req.write(tmpl.render(context))
+
+
+def repository_diff_message(lastChange):
+	project_date_diff = time.time() - lastChange.date
+	nbYears = project_date_diff / (60 * 60 * 24 * 30 * 12)
+	if nbYears > 1:
+		project_message_diff = " plus de " + str(int(nbYears)) + " an"
+	else:
+		nbMonths = project_date_diff / (60 * 60 * 24 * 30)
+		if nbMonths > 1:
+			project_message_diff = str(int(nbMonths)) + " mois"
+		else:
+			nbDays = project_date_diff / (60 * 60 * 24)
+			if nbDays > 1:
+				project_message_diff = str(int(nbDays)) + " jours "
+			else:
+				nbHours = project_date_diff / (60 * 60)
+				if nbHours > 1:
+					project_message_diff = "plus de " + str(int(nbHours)) + " heures"
+				else:
+					project_message_diff = "moins d'une heure"
+	return project_message_diff
+
 
 def view_index(req, config):
-	"Index des projets"
+	"""Index des projets"""
 	req.content_type = 'text/html'
-	now = time.time()
 	tracProjects = TracProjects(config.trac_dir)
 	projects = []
 	for project in tracProjects:
+		# Prepare variable to set project
+		env = project.get_env()
+		repos = project.get_repos()
+		dir = path.basename(project.path)
 		try:
-			env = project.get_env()
-			repos = project.get_repos()
-			#  
 			lastChange = repos.get_changeset(repos.youngest_rev)
-			dateDiff = now - lastChange.date
-			nbYears = dateDiff / (60 * 60 * 24 * 30 * 12)
-			# TODO: Generique
-			if nbYears > 1:
-				messageDiff = " plus de " + str(int(bYears)) + " an"
-			else:
-				nbMonths = dateDiff / (60 * 60 * 24 * 30)
-				if nbMonths > 1:
-					messageDiff = str(int(nbMonths)) + " mois"
-				else:
-					nbDays = dateDiff / (60 * 60 * 24)
-					if nbDays > 1:
-						messageDiff = str(int(nbDays)) + " jours "
-					else:
-						nbHours = dateDiff / (60 * 60)
-						if nbHours > 1:
-							messageDiff = "plus de " + str(int(nbHours)) + " heures"
-						else:
-							messageDiff = "moins d'une heure"
-			dir = path.basename(project.path)
-			# DEMO
-			demo = False
-			if path.exists("%s/%s" % (config.demo_dir, dir)):
-				demo = True
-			# TARBALL
-			tarball = False
-			if path.exists("%s/%s" % (config.archives_dir, dir)):
-				tarball = True
-			proj = {'name' : env.project_name,
-				'href' : config.trac_href + dir,
-				'description' : env.project_description,
-				'activity' : {
-					'lastrev' : repos.youngest_rev,
-					'lastauthor' : lastChange.author,
-					'lastdate' : messageDiff,
-					'lastmessage' : lastChange.message,
-					'datediff' : dateDiff
-				},
-				'demo' : {
-					'enable' : demo,
-					'href' : "%s/%s" % (config.demo_href, dir)
-				},
-				'tarball' : {
-					'enable' : tarball,
-					'href' : "%s/%s" % (config.archives_href, dir)
-				}
+			project_name = env.project_name
+			project_href = config.trac_href + dir
+			project_description = env.project_description
+			project_youngest_rev = repos.youngest_rev
+			project_last_author = lastChange.author
+			project_last_message = lastChange.message
+			project_laste_date = lastChange.date
+			project_date_diff = time.time() - lastChange.date
+			project_message_diff = repository_diff_message(lastChange)
+		# in the case of the repository has not yet been modified
+		except:
+			project_last_author = ""
+			project_last_message = "This repository has not yet been modified"
+			project_laste_date = time.time()
+			project_name = env.project_name
+			project_href = config.trac_href + dir
+			project_description = env.project_description
+			project_youngest_rev = "0"
+			project_date_diff = time.time() - time.time()
+			project_message_diff = "0 minutes"
+		# DEMO
+		demo = False
+		if path.exists("%s/%s" % (config.demo_dir, dir)):
+			demo = True
+		# TARBALL
+		tarball = False
+		if path.exists("%s/%s" % (config.archives_dir, dir)):
+			tarball = True
+		# a project
+		proj = {'name' : project_name,
+			'href' : project_href,
+			'description' : project_description,
+			'activity' : {
+				'lastrev' : project_youngest_rev,
+				'lastauthor' : project_last_author,
+				'lastdate' : project_message_diff,
+				'lastmessage' : project_last_message,
+				'datediff' : project_date_diff
+			},
+			'demo' : {
+				'enable' : demo,
+				'href' : "%s/%s" % (config.demo_href, dir)
+			},
+			'tarball' : {
+				'enable' : tarball,
+				'href' : "%s/%s" % (config.archives_href, dir)
 			}
-			projects.append(proj)
-		except Exception, e:
-			pass
-	if req.uri == "/a":
+		}
+		projects.append(proj)
+	# sort projects
+	if req.uri == config.forge_href+"/a":
 		projects.sort(lambda x,y: cmp(x['name'], y['name']))
 		sort = "a"
-	elif req.uri == "/z":
+	elif req.uri == config.forge_href+"/z":
 		projects.sort(lambda x,y: cmp(y['name'], x['name']))
 		sort = "z"
 	else:
 		projects.sort(lambda x, y: cmp(x['activity']['datediff'], y['activity']['datediff']))
 		sort = "b"
-	t = Template('projects', FileSystemLoader(config.template_dir))
-	c = Context({
+	# apply template
+	environment = Environment(loader=FileSystemLoader(config.template_dir))
+	template = environment.get_template("projects.html")
+	context = {
 		'projects' : projects,
 		'sort' : sort,
-		'forge_href' : config.forge_href
-	})
-	req.write(t.render(c))
+		'forge_href' : config.forge_href,
+		'media_href' : config.media_href,
+		'debug' : req.uri
+	}
+	req.write(template.render(context))
 
 
