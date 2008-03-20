e0ec9c187a4098152a1c25080a0b00ff4c2ffafe
Stephan Gorget <phantez@gmail.com>
2008-03-20 16:07:58 +0000

    example on howto create trac&svn environment with a python script
    git-svn-id: http://forge.2metz.fr/svn/trac-forge/trunk@7 671e0883-ef23-4d3e-8a5f-78a0a4f73774

---
+++ tracenvadmin.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#
+# trac-admin initenv <projectname> <db> <repostype> <repospath> <templatepath>
+#
+import os
+import time
+
+class Tracenvadmin:
+
+	def __init__(self):
+		self.db = "sqlite:db/trac.db"
+		self.template = "/usr/share/trac/templates"
+		self.rootpath = "/var/www/tracforge"
+		self.tracpath = self.rootpath+"/trac/"
+		self.svnpath = self.rootpath+"/svn/"
+		self.svn = "svn"
+	
+	def createenv(self,projectname = None):
+		if (!projectname):
+			raise ProjectnameUndefined
+		os.chdir(self.svnpath)
+		os.system("/usr/bin/svnadmin create "+projectname)
+		os.chdir(self.tracpath)	
+		os.system("/usr/bin/trac-admin "+projectname+" initenv "+projectname+" "+self.db+" "+self.svn+" "+self.svnpath+projectname+" "+self.template)
+
+
+if __name__ == "__main__":
+	env = Tracenvadmin()
+	env.createenv()
+
